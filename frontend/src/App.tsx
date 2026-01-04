30db776b5438008e83ca87f08cce371bc8b17b16
SpringPurpur <vaceffv@gmail.com>
2026-01-04 22:20:49 +0200

    feat: added axios instance for communicating with backend. Added finishing touches

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import JoinSession from './pages/JoinSession';
+import StudentView from './pages/Student/StudentView';
+import Register from './pages/Teacher/Register';
+import Login from './pages/Teacher/Login';
+import ActivitySession from './pages/Teacher/ActivitySession';
+import TeacherDashboard from './pages/Teacher/TeacherDashboard';
+import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar';
+import NotFound from './pages/NotFound';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen flex flex-col">
+        <Navbar /> {/* <--- Aici apare pe toate paginile */}
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path='join' element={<JoinSession />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            
+            <Route path="/teacher" element={<PrivateRoute><TeacherDashboard /></PrivateRoute>} />
+            <Route path="/teacher/activity/:id" element={<PrivateRoute><ActivitySession /></PrivateRoute>} />
+            
+            {/* Vizualizarea studentului - Navbar-ul se va ascunde automat aici */}
+            <Route path="/session/:code" element={<StudentView />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/api/axiosInstance.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:5000/api',
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token && config.headers) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config; 
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
